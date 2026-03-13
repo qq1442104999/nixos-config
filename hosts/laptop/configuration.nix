228f14f35d04288fe879d4d259f504280224735e
wang <1442104999@qq.com>
2026-03-14 01:10:38 +0800

    1

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -20,7 +20,7 @@
   # ==================================================
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  boot.loader.efi.efiSysMountPoint = "/boot";
 
   # ==================================================
   # 网络

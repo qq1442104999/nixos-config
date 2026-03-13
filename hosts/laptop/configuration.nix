25ac7f908e8296a1dd754a5e7c1472f668473a84
wang <1442104999@qq.com>
2026-03-13 23:28:58 +0800

    1

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -35,9 +35,11 @@
   systemd.services."NetworkManager".enable = true;
 
   networking.networkmanager.wifi = {
-    "H3C_A96AFE_5G" = {
-      password = "80808080";
-      priority = 1;  # 优先级高的优先连接
+    networks = {
+      "H3C_A96AFE_5G" = {
+        password = "80808080";
+        priority = 1;  # 优先级高的优先连接
+      };
     };
   };
 

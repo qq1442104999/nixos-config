b13ea9eec6c1711871b2563bfe4e9aca8965219c
wang <1442104999@qq.com>
2026-03-20 01:06:02 +0800

    1

--- flake.nix
+++ flake.nix
@@ -44,7 +44,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    imports = [ modules/home/omzsh/flake_inputs.nix];
+    imports = [ ./modules/home/omzsh/flake_inputs.nix ];
   };
 
   # 2️⃣ 输出

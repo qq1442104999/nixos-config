3af1a5e694bd038ebf5c0063d5c13c3a9208d3ad
wang <1442104999@qq.com>
2026-03-20 01:04:31 +0800

    1

--- flake.nix
+++ flake.nix
@@ -43,6 +43,8 @@
       url = "github:sodiboo/niri-flake";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    imports = [ modules/home/omzsh/flake_inputs.nix];
   };
 
   # 2️⃣ 输出
---
+++ modules/home/omzsh/flake_inputs.nix
@@ -0,0 +1,6 @@
+{
+  fzf-tab-src = {
+    url = "github:Aloxaf/fzf-tab";
+    flake = false; # 它不是一个 flake 仓库，只是纯源码
+  };
+}

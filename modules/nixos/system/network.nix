eba23890c9d629af352de7f3183ff29a363059dc
wang <1442104999@qq.com>
2026-03-19 22:56:24 +0800

    1

--- modules/nixos/system/network.nix
+++ modules/nixos/system/network.nix
@@ -5,7 +5,7 @@
   # 这里建议只写通用 NM 配置，具体的 Wi-Fi Profile 可以留在 host 级
   services.openssh = {
     enable = true;
-    settings.PasswordAuthentication = false;
+    settings.PasswordAuthentication = true;
   };
 
   users.users.wang.openssh.authorizedKeys.keys = [

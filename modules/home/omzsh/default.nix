560cc53a47954a37665bb1f482afdf0d0d7d95c0
wang <1442104999@qq.com>
2026-03-21 01:17:56 +0800

    1

--- modules/home/omzsh/default.nix
+++ modules/home/omzsh/default.nix
@@ -167,8 +167,21 @@ in
       setopt prompt_subst
 
       # ======================
-      # FZF tab配置
+      # FZF 配置
       # ======================
+      #export FZF_DEFAULT_COMMAND='fd --hidden --follow --exclude .git'
+      #export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+      #export FZF_ALT_C_COMMAND='fd --type d --hidden --follow --exclude .git'
+
+      #export FZF_CTRL_T_OPTS="--preview '${./scripts/preview.sh} {}'"
+      #export FZF_DEFAULT_OPTS="
+      #  --height=40%
+      #  --layout=reverse
+      #  --border
+      #  --info=inline
+      #  --preview-window=right:60%
+      #"
+
       zstyle ':fzf-tab:*' fzf-command fzf
       export FZF_TAB_CUSTOM_PREVIEW="${./scripts/preview.sh} {}"
 

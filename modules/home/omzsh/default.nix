da090d7848e2477ffe59610fdd449078e931e756
wang <1442104999@qq.com>
2026-03-21 20:19:22 +0800

    1

--- modules/home/omzsh/default.nix
+++ modules/home/omzsh/default.nix
@@ -85,32 +85,26 @@ in
       }
     ];
 
+    setOptions = [ ];
+
     # ----------------------
     # initExtra 拆块
     # ----------------------
     initContent = lib.mkOrder 1000 ''
       # ========= 环境变量 =========
       export LESS='-R'
-
-      setopt GLOB_DOTS
-      zstyle ':completion:*' menu no
-
       SHORT_HOST=''${HOST/.*/}
+      zstyle ':completion:*' menu no
       autoload -Uz add-zsh-hook
       zmodload -i zsh/complist
+      autoload -U colors && colors
+
       unsetopt correct
+      setopt GLOB_DOTS
+
       setopt auto_pushd
       setopt pushd_ignore_dups
       setopt pushdminus
-
-      function d () {
-        if [[ -n $1 ]]; then
-          dirs "$@"
-        else
-          dirs -v | head -10
-        fi
-      }
-      compdef _dirs d
 
       setopt extended_history
       setopt hist_expire_dups_first
@@ -120,6 +114,21 @@ in
       setopt inc_append_history
       setopt share_history
 
+      setopt auto_cd
+      setopt multios
+      setopt prompt_subst
+
+      # ========= 自定义函数 =========
+      function d () {
+        if [[ -n $1 ]]; then
+          dirs "$@"
+        else
+          dirs -v | head -10
+        fi
+      }
+      compdef _dirs d
+
+      # ========= OMZ zle 初始化 smkx/rmkx =========
       if (( ''${+terminfo[smkx]} )) && (( ''${+terminfo[rmkx]} )); then
         function zle-line-init() {
           echoti smkx
@@ -130,53 +139,51 @@ in
         zle -N zle-line-init
         zle -N zle-line-finish
       fi
-      bindkey -e                                            # Use emacs key bindings
-      bindkey '^r' history-incremental-search-backward      # [Ctrl-r] - Search backward incrementally for a specified string. The string may begin with ^ to anchor the search to the beginning of the line.
-      if [[ "''${terminfo[kpp]}" != "" ]]; then
-        bindkey "''${terminfo[kpp]}" up-line-or-history       # [PageUp] - Up a line of history
-      fi
-      if [[ "''${terminfo[knp]}" != "" ]]; then
-        bindkey "''${terminfo[knp]}" down-line-or-history     # [PageDown] - Down a line of history
-      fi
-      if [[ "''${terminfo[kcuu1]}" != "" ]]; then
-        autoload -U up-line-or-beginning-search
-        zle -N up-line-or-beginning-search
-        bindkey "''${terminfo[kcuu1]}" up-line-or-beginning-search
-      fi
-      if [[ "''${terminfo[kcud1]}" != "" ]]; then
-        autoload -U down-line-or-beginning-search
-        zle -N down-line-or-beginning-search
-        bindkey "''${terminfo[kcud1]}" down-line-or-beginning-search
-      fi
-      if [[ "''${terminfo[khome]}" != "" ]]; then
-        bindkey "''${terminfo[khome]}" beginning-of-line      # [Home] - Go to beginning of line
-      fi
-      if [[ "''${terminfo[kend]}" != "" ]]; then
-        bindkey "''${terminfo[kend]}"  end-of-line            # [End] - Go to end of line
-      fi
-      bindkey ' ' magic-space                               # [Space] - do history expansion
-      bindkey '^[[1;5C' forward-word                        # [Ctrl-RightArrow] - move forward one word
-      bindkey '^[[1;5D' backward-word                       # [Ctrl-LeftArrow] - move backward one word
-      if [[ "''${terminfo[kcbt]}" != "" ]]; then
-        bindkey "''${terminfo[kcbt]}" reverse-menu-complete   # [Shift-Tab] - move through the completion menu backwards
-      fi
-      bindkey '^?' backward-delete-char                     # [Backspace] - delete backward
-      if [[ "''${terminfo[kdch1]}" != "" ]]; then
-        bindkey "''${terminfo[kdch1]}" delete-char            # [Delete] - delete forward
-      else
-        bindkey "^[[3~" delete-char
-        bindkey "^[3;5~" delete-char
-        bindkey "\e[3~" delete-char
-      fi
 
-      autoload -U colors && colors
-      setopt auto_cd
-      setopt multios
-      setopt prompt_subst
+      # ========= 快捷键绑定 =========
+      _apply_keybindings() {
+        bindkey -e                                            # Use emacs key bindings
+        bindkey '^r' history-incremental-search-backward      # [Ctrl-r] - Search backward incrementally for a specified string. The string may begin with ^ to anchor the search to the beginning of the line.
+        if [[ "''${terminfo[kpp]}" != "" ]]; then
+          bindkey "''${terminfo[kpp]}" up-line-or-history       # [PageUp] - Up a line of history
+        fi
+        if [[ "''${terminfo[knp]}" != "" ]]; then
+          bindkey "''${terminfo[knp]}" down-line-or-history     # [PageDown] - Down a line of history
+        fi
+        if [[ "''${terminfo[kcuu1]}" != "" ]]; then
+          autoload -U up-line-or-beginning-search
+          zle -N up-line-or-beginning-search
+          bindkey "''${terminfo[kcuu1]}" up-line-or-beginning-search
+        fi
+        if [[ "''${terminfo[kcud1]}" != "" ]]; then
+          autoload -U down-line-or-beginning-search
+          zle -N down-line-or-beginning-search
+          bindkey "''${terminfo[kcud1]}" down-line-or-beginning-search
+        fi
+        if [[ "''${terminfo[khome]}" != "" ]]; then
+          bindkey "''${terminfo[khome]}" beginning-of-line      # [Home] - Go to beginning of line
+        fi
+        if [[ "''${terminfo[kend]}" != "" ]]; then
+          bindkey "''${terminfo[kend]}"  end-of-line            # [End] - Go to end of line
+        fi
+        bindkey ' ' magic-space                               # [Space] - do history expansion
+        bindkey '^[[1;5C' forward-word                        # [Ctrl-RightArrow] - move forward one word
+        bindkey '^[[1;5D' backward-word                       # [Ctrl-LeftArrow] - move backward one word
+        if [[ "''${terminfo[kcbt]}" != "" ]]; then
+          bindkey "''${terminfo[kcbt]}" reverse-menu-complete   # [Shift-Tab] - move through the completion menu backwards
+        fi
+        bindkey '^?' backward-delete-char                     # [Backspace] - delete backward
+        if [[ "''${terminfo[kdch1]}" != "" ]]; then
+          bindkey "''${terminfo[kdch1]}" delete-char            # [Delete] - delete forward
+        else
+          bindkey "^[[3~" delete-char
+          bindkey "^[3;5~" delete-char
+          bindkey "\e[3~" delete-char
+        fi
+        }
+      _apply_keybindings
 
-      # ======================
-      # FZF 配置
-      # ======================
+      # ========= FZF-Tab 自定义预览 =========
       #export FZF_DEFAULT_COMMAND='fd --hidden --follow --exclude .git'
       #export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
       #export FZF_ALT_C_COMMAND='fd --type d --hidden --follow --exclude .git'

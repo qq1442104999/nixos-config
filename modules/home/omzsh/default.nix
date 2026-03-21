320d0ef026a4b1586f57ef4f7189ee39c6612a86
wang <1442104999@qq.com>
2026-03-21 19:36:25 +0800

    1

--- modules/home/omzsh/default.nix
+++ modules/home/omzsh/default.nix
@@ -126,7 +126,7 @@ in
         zle -N zle-line-finish
       fi
       bindkey -e                                            # Use emacs key bindings
-      #bindkey '^r' history-incremental-search-backward      # [Ctrl-r] - Search backward incrementally for a specified string. The string may begin with ^ to anchor the search to the beginning of the line.
+      bindkey '^r' history-incremental-search-backward      # [Ctrl-r] - Search backward incrementally for a specified string. The string may begin with ^ to anchor the search to the beginning of the line.
       if [[ "''${terminfo[kpp]}" != "" ]]; then
         bindkey "''${terminfo[kpp]}" up-line-or-history       # [PageUp] - Up a line of history
       fi

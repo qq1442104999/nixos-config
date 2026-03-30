3c77d65db6277d6f9cfb033f30dfb03466848d79
wang <1442104999@qq.com>
2026-03-30 21:50:02 +0800

    1

--- modules/home/omzsh/programs/tmux.nix
+++ modules/home/omzsh/programs/tmux.nix
@@ -10,12 +10,9 @@ in
 {
   programs.tmux = {
     enable = true;
-    #extraConfig = (
-    #  builtins.readFile (
-    #    pkgs.replaceVars ./scripts/my_initContent.zsh{
-    #      ZSH_CACHE_DIR = "${config.home.homeDirectory}/.cache/oh-my-zsh"; # Nix 会自动把这个路径替换成 /nix/store/...
-    #    }
-    #  )
-    #);
+    extraConfig = 
+      builtins.readFile "${tmuxConf}/.tmux.conf";
   };
+
+  xdg.configFile."/tmux/.tmux.conf.local".source = ../scripts/tmux.conf.local;
 }

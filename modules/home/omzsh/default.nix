12a691f62f08284b1ead3ba698a4dc4234b5df2e
wang <1442104999@qq.com>
2026-03-30 16:11:58 +0800

    1

--- modules/home/omzsh/default.nix
+++ modules/home/omzsh/default.nix
@@ -101,10 +101,11 @@ in
     # initExtra 拆块
     # ----------------------
     initContent = lib.mkOrder 1000 (
-      builtins.readFile (pkgs.substituteAll {
-        src = ./your-zsh-config.sh;
-        ZSH_CACHE_DIR = "${config.home.homeDirectory}/.cache/oh-my-zsh"; # Nix 会自动把这个路径替换成 /nix/store/...
-      });
+      builtins.readFile (
+        pkgs.replaceVars ./scripts/my_initContent.zsh{
+          ZSH_CACHE_DIR = "${config.home.homeDirectory}/.cache/oh-my-zsh"; # Nix 会自动把这个路径替换成 /nix/store/...
+        }
+      )
     );
 
   };

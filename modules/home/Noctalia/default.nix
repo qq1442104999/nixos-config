a104de97bd01ba169204330d25a7a355f68d59ed
wang <1442104999@qq.com>
2026-04-02 22:16:28 +0800

    1

--- modules/home/Noctalia/default.nix
+++ modules/home/Noctalia/default.nix
@@ -5,66 +5,16 @@
   ];
   # configure options
   programs.noctalia-shell.enable = true;
-    #enable = true;
-    #settings = {
-    #  # configure noctalia here
-    #  bar = {
-    #    density = "compact";
-    #    position = "right";
-    #    showCapsule = false;
-    #    widgets = {
-    #      left = [
-    #        {
-    #          id = "ControlCenter";
-    #          useDistroLogo = true;
-    #        }
-    #        {
-    #          id = "Network";
-    #        }
-    #        {
-    #          id = "Bluetooth";
-    #        }
-    #      ];
-    #      center = [
-    #        {
-    #          hideUnoccupied = false;
-    #          id = "Workspace";
-    #          labelMode = "none";
-    #        }
-    #      ];
-    #      right = [
-    #        {
-    #          alwaysShowPercentage = false;
-    #          id = "Battery";
-    #          warningThreshold = 30;
-    #        }
-    #        {
-    #          formatHorizontal = "HH:mm";
-    #          formatVertical = "HH mm";
-    #          id = "Clock";
-    #          useMonospacedFont = true;
-    #          usePrimaryColor = true;
-    #        }
-    #      ];
-    #    };
-    #  };
-    #  colorSchemes.predefinedScheme = "Monochrome";
-    #  general = {
-    #    avatarImage = "/home/drfoobar/.face";
-    #    radiusRatio = 0.2;
-    #  };
-    #  location = {
-    #    monthBeforeDay = true;
-    #    name = "Marseille, France";
-    #  };
-    #};
-    # this may also be a string or a path to a JSON file.
-  #};
 
+
+
+  #xdg.configFile."noctalia/settings.json".source =
+  #  pkgs.lib.mkForce (
+  #    config.lib.file.mkOutOfStoreSymlink 
+  #      "${toString ./settings.json}"
+  #  );
 
   xdg.configFile."noctalia/settings.json".source =
-    pkgs.lib.mkForce (
-      config.lib.file.mkOutOfStoreSymlink 
-        "${toString ./settings.json}"
-    );
+    config.lib.file.mkOutOfStoreSymlink 
+      "${toString ./settings.json}";
 }

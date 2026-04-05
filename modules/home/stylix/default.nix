02299b2141f06eaf6fe2fb7093666b3355c58925
wang <1442104999@qq.com>
2026-04-05 19:23:36 +0800

    1

--- modules/home/stylix/default.nix
+++ modules/home/stylix/default.nix
@@ -9,6 +9,30 @@
 
     base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
 
+    fonts = {
+      monospace = {
+        package = pkgs.nerd-fonts.jetbrains-mono;
+        name = "JetBrainsMono Nerd Font";
+      };
+      sansSerif = {
+        package = pkgs.source-han-sans;
+        name = "Source Han Sans SC";
+      };
+      serif = {
+        package = pkgs.source-han-sans;
+        name = "Source Han Sans SC";
+      };
+      emoji = {
+        package = pkgs.noto-fonts-emoji;
+        name = "Noto Color Emoji";
+      };
+      sizes = {
+        application = 12;
+        desktop = 10;
+        terminal = 12;
+      };
+    };
+
     targets = {
       noctalia-shell = {
         enable = false;
@@ -17,7 +41,7 @@
         enable = true;
         plugin = "base16-nvim";
       };
-
+      kde.enable = false;
     };
   };
 
--- modules/nixos/system/fonts.nix
+++ modules/nixos/system/fonts.nix
@@ -5,11 +5,11 @@
     source-han-sans            # 思源黑体
   ];
 
-  fonts.fontconfig.enable = true;
+  #fonts.fontconfig.enable = true;
 
-  fonts.fontconfig.defaultFonts = {
-    monospace = [ "JetBrainsMono Nerd Font" "Source Han Sans" ];
-    sansSerif = [ "Source Han Sans" ];
-    serif     = [ "Source Han Sans" ];
-  };
+  #fonts.fontconfig.defaultFonts = {
+  #  monospace = [ "JetBrainsMono Nerd Font" "Source Han Sans SC" ];
+  #  sansSerif = [ "Source Han Sans SC" ];
+  #  serif     = [ "Source Han Sans SC" ];
+  #};
 }

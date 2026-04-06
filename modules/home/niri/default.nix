bffec52df2019c0bd78b6313f83928a96db723b0
wang <1442104999@qq.com>
2026-04-06 22:58:14 +0800

    1

--- modules/home/niri/default.nix
+++ modules/home/niri/default.nix
@@ -9,15 +9,15 @@
   #  enable = true;
   #};
 
-  home.file.".config/niri/config.kdl" = {
-    source = config.lib.file.mkOutOfStoreSymlink 
-      "${config.home.homeDirectory}/nixos/modules/home/niri/config.kdl";
-    recursive = false;
-  };
+  home.file.".config/niri/config.kdl".source = 
+    config.lib.file.mkOutOfStoreSymlink 
+      "/home/wang/nixos/modules/home/niri/config.kdl";
 
-  #xdg.configFile."niri/config.kdl".source =
-  #  config.lib.file.mkOutOfStoreSymlink 
-  #    "${config.home.homeDirectory}/nixos/modules/home/niri/config.kdl";
+      # 使用 home.file 并强制覆盖
+  #home.file.".config/niri/config.kdl" = {
+  #  source = lib.mkForce (config.lib.file.mkOutOfStoreSymlink 
+  #    "${config.home.homeDirectory}/nixos/modules/home/niri/config.kdl");
+  #};
 
   home.packages = with pkgs; [
     kitty

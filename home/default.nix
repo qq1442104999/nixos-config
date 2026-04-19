2abf8abee23203a8562367dfc8087e71f2e7d1da
wang <1442104999@qq.com>
2026-04-20 00:00:10 +0800

    1

--- home/default.nix
+++ home/default.nix
@@ -18,12 +18,13 @@
   ];
 
   stylix.targets.vscode.enable = false;
-  
+
   home.packages = with pkgs; [
     kitty       # 终端
     fastfetch   # 系统信息工具
     pciutils    # lspci 工具
     tofi        # 应用启动器
+    nvfetcher   # 插件管理工具
 
     vlc         # 视频播放器
     imagemagick # 图像处理工具

283d406ce35dbf161e627f284845cdb37b206611
wang <1442104999@qq.com>
2026-04-19 22:35:40 +0800

    1

--- modules/home/programs/vscode.nix
+++ modules/home/programs/vscode.nix
@@ -3,8 +3,29 @@
   programs.vscode = {
     enable = true;
 
-    profiles.default.extensions = with pkgs.vscode-extensions; [
-      ms-ceintl.vscode-language-pack-zh-hans
-    ];
+    profiles.default = {
+      extensions = with pkgs.vscode-extensions; [
+        ms-ceintl.vscode-language-pack-zh-hans
+        jnoortheen.nix-ide
+        catppuccin.catppuccin-vsc
+        pkief.material-icon-theme
+      ];
+
+      userSettings = {
+        "locale" = "zh-cn";
+        #"workbench.colorTheme" = "Catppuccin Dark";
+        "workbench.iconTheme" = "Material Icon Theme";
+        #"editor.fontFamily" = "'Fira Code', 'Source Code Pro', Consolas, 'Courier New', monospace";
+        "editor.fontLigatures" = true;
+        #"editor.fontSize" = 14;
+        "editor.lineHeight" = 22;
+        "editor.tabSize" = 2;
+        "editor.renderWhitespace" = "all";
+        "editor.minimap.enabled" = false;
+        "files.trimTrailingWhitespace" = true;
+        "files.insertFinalNewline" = true;
+        "files.autoSave" = "onFocusChange";
+      };
+    };
   };
 }

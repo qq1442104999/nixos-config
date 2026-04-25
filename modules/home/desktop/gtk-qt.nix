d7891b01b37e03e0ed6f9dca76e3c5175c17f91b
wang <1442104999@qq.com>
2026-04-25 23:23:04 +0800

    1

--- modules/home/desktop/gtk-qt.nix
+++ modules/home/desktop/gtk-qt.nix
@@ -1,16 +1,5 @@
 { pkgs, ... }:
 {
-  # 1. 安装必要的包
-  home.packages = with pkgs; [
-    libsForQt5.qt5ct
-    kdePackages.qt6ct
-    
-    libsForQt5.qtstyleplugin-kvantum
-    kdePackages.qtstyleplugin-kvantum
-
-    nwg-look
-  ];
-
   home.pointerCursor = {
     package = pkgs.bibata-cursors;
     name = "Bibata-Modern-Ice";
@@ -42,16 +31,4 @@
       name = "catppuccin-mocha-lavender-standard";
     };
   };
-
-  home.sessionVariables = {
-    QT_QPA_PLATFORM = "wayland;xcb";
-    QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
-  };
-
-  #environment.variables = {
-  #  QT_QPA_PLATFORM = "wayland";
-  #  QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
-  #  QT_X11_NO_MITSHM = "1";
-  #  NIXOS_OZONE_WL = "1";
-  #};
 }
